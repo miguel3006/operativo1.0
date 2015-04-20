30c63da136b984da05e8dad1139da9fd52692b8c
sandok28 <edwinsandoval.428@gmail.com>
2015-04-20 18:30:29 -0500

    iojs

---
+++ iojs/server.js
@@ -0,0 +1,12 @@
+var app = require('express')();
+var http = require('http').Server(app); 
+var io = require('socket.io')(http);
+
+io.on('connection',function(socket){
+console.log('usuario conectado ');
+});
+
+http.listen(3000,function(){
+console.log(' conetado por http://127.0.0.1:3000');
+});
+
